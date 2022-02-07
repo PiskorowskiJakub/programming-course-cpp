5748f670ff77b06bea85f81db05855d437bd3429
Jakub Piskorowski <jakub9616@gmail.com>
2022-02-07 10:41:13 +0100

    Added solution to 124 and 125

---
+++ 1-programowanie-strukturalne/1-2-instrukcje-sterujace/1-2-4-do-while/124-zadania/zad2.cpp
@@ -0,0 +1,28 @@
+//  zad2.cpp
+//  1-programowanie-strukturalne\1-2-instrukcje-sterujace\1-2-4-do-while\124-zadania
+//
+//  Created by Jakub Piskorowski on 12/01/2022 wersja: 1.0
+//  Copyright © 2021 Jakub Piskorowski. All rights reserved.
+//	GitHub: https://github.com/PiskorowskiJakub/programming-course-cpp
+//	
+//  Przedstawienie dzialania petli do while
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ile;
+   
+    cout << "Ile razy wypisac napis? ";
+    cin >> ile;
+    int pozycja = 1;
+    if( ile > 0 )
+    do
+    {
+        cout << "Napis nr" << pozycja << endl;
+        pozycja++;
+        ile--;
+    } while( ile > 0 );
+   
+    return 0;
+}
---
+++ 1-programowanie-strukturalne/1-2-instrukcje-sterujace/1-2-4-do-while/124-zadania/zad3.cpp
@@ -0,0 +1,24 @@
+//  zad3.cpp
+//  1-programowanie-strukturalne\1-2-instrukcje-sterujace\1-2-4-do-while\124-zadania
+//
+//  Created by Jakub Piskorowski on 12/01/2022 wersja: 1.0
+//  Copyright © 2021 Jakub Piskorowski. All rights reserved.
+//	GitHub: https://github.com/PiskorowskiJakub/programming-course-cpp
+//	
+//  Przedstawienie dzialania petli do while
+//
+#include <iostream>
+using namespace std;
+int main()
+{
+    int liczba;
+    do
+    {
+        cout << "Podaj liczbe: " << endl;
+        cin >> liczba;
+
+    } while( liczba != 4 );
+   
+    cout << "Wpisales liczbe" << liczba << "Koniec programu" << endl;
+    return 0;
+}
