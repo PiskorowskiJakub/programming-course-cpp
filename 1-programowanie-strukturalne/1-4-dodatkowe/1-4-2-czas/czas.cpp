e87b16dab3de53e9607aacccdfc43077eacd74f5
Jakub Piskorowski <jakub9616@gmail.com>
2022-03-30 13:51:56 +0200

    Czas

---
+++ 1-programowanie-strukturalne/1-4-dodatkowe/1-4-2-czas/czas.cpp
@@ -0,0 +1,45 @@
+//
+//  czas.cpp
+//  1-programowanie-strukturalne\1-4-dodatkowe\1-4-2-czas
+//
+//  Created by Jakub Piskorowski on 30/03/2022 wersja: 1.0
+//  Copyright © 2021 Jakub Piskorowski. All rights reserved.
+//	GitHub: https://github.com/PiskorowskiJakub/programming-course-cpp
+//	
+//  Przedstawienie dzialania funkcji losujacej
+//
+
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+chrono::duration<double> elapsed_seconds;
+
+int main() {
+	chrono::system_clock::time_point start = chrono::system_clock::now();	// czas start
+
+	int sum=0; int add = 1;
+	int iterations = 1000*100*50;
+	float wynik;
+	cout << "iteration: " << iterations << endl;
+	
+	for (int i=0; i<=iterations; i++) {
+		if(i % 1000 == 0){
+			wynik =(i*100)/iterations;
+			system("cls");
+			cout<< "Progres: "<< wynik<< " % ";	
+		}
+    }
+	chrono::system_clock::time_point end = chrono::system_clock::now(); // czas stop
+
+	elapsed_seconds += end - start;	// obliczenie czasu
+	cout << "\nCzas: " << endl;
+	// wyswietlenie czasu w roznych jednostkach
+	cout << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_seconds).count()<< "\t milliseconds\n";
+	cout << std::chrono::duration_cast<std::chrono::seconds>(elapsed_seconds).count()<< "\t seconds\n";
+	//cout <<  elapsed_seconds.count() << " second" << endl;
+	cout << std::chrono::duration_cast<std::chrono::minutes>(elapsed_seconds).count()<< "\t minutes\n";
+	
+	
+
+}
--- 1-programowanie-strukturalne/1-4-dodatkowe/README.md
+++ 1-programowanie-strukturalne/1-4-dodatkowe/README.md
@@ -17,5 +17,6 @@
 1 [Programowanie strukturalne](/1-programowanie-strukturalne/README.md) (powrót) \
 &emsp;4.0. [Zadania](14-zadania/README.md) \
 &emsp;&emsp;4.0.1 [Zadanie 1 - Menu](14-zadania/README.md#zadanie-1---menu) \
-&emsp;4.1. [Losowanie liczb](1-4-1-losowanie/losowanie.cpp)
+&emsp;4.1. [Losowanie liczb](1-4-1-losowanie/losowanie.cpp) \
+&emsp;4.2. [Czas](1-4-2-czas/czas.cpp)
 
